6335266b320bb3e329825253c5e10694c08e7ced
Martin Albrecht <albrecht@linkbird.com>
2014-01-15 13:42:17 +0100

    unfinished new version

--- remainingChars.js
+++ remainingChars.js
@@ -1,19 +1,13 @@
 /**
- * Simple object to calculate the remaining chars in a textfield,
- * chop the text if maximum is reached and print the status.
- * 
- * NOTE: This object ist based on jQuery, so include that in your
- *       code before this class.
+ * Simple class to calculate the remaining chars in a textfield or input field.
  *
  * Valid option parameters:
  * ------------------------
- *   maxChars: The count of maximum chars allowed {required}
- *   textarea: The textarea element, id or class string {required}
- *   statusarea: The status area element, id or class string {required}
  *   statustext: The text to show in the status area {optional}
  *               If no text is passed the default message is taken.
  *               IMPORTANT: The statustext string must include a %d for being
- *                          replaced with the count of remaining characters.
+ *                          replaced with the count of remaining characters
+ *                          (e.g. "Remaining: %d").
  *    
  * @author Martin Albrecht <martin.albrecht@javacoffee.de> 
  * @param {object} opts Option array
@@ -21,61 +15,68 @@
  */
 function remainingChars(opts) {
   var self = this;
-  
-  // Check options
-  if( typeof opts.maxChars === undefined || opts.maxChars <= 0 ) {
-    alert('Please define a valid maxChar option!');
-    return false;
-  } else {
-    this.maxChars = opts.maxChars;
-  }
-  
-  if( typeof opts.textarea === undefined ) {
-    alert('textarea option needed!');
-    return false;
-  }
-        
-  this.textarea = $(opts.textarea);
-  if( this.textarea.position() === undefined || this.textarea.length > 1) {
-    alert('Please give a valid textarea option (id, this, class)!');
-    return false;
-  }
-  
-  if( typeof opts.statusarea === undefined ) {
-    alert('statusarea option needed!');
-    return false;
-  }
-  
-  this.statusarea = $(opts.statusarea);
-  if( this.statusarea.position() === undefined || this.statusarea.length > 1) {
-    alert('Please give a valid statusarea option (id, this, class)!');
-    return false;
-  }          
-  
-  if( typeof opts.statustext === undefined || opts.statustext.search(/%d/) === -1 ) {
-    this.statustext = 'Remaining: %d';
-  } else {
-    this.statustext = opts.statustext;
-  }            
-  
-  // Show the status message
-  this.showStatus = function(remaining) {
-    this.statusarea.text(self.statustext.replace(/%d/, remaining)).html();
+
+  this.insertAfter = function insertAfter(element, target) {  
+      var parent = target.parentNode;
+      if(parent.lastchild == target) {    
+          parent.appendChild(element);
+      } else {    
+          parent.insertBefore(element, target.nextSibling);
+      }
   };
-  
-  // Bind textarea to change events and set statustext      
-  this.textarea.on('change keyup', function(){          
-    var actualCount = self.textarea.val().length;               
-    
-    if(actualCount > self.maxChars) {
-      var newVal = $(this).val().substring(0, self.maxChars);
-      $(this).val(newVal);          
+
+  this.appendCharCount = function(element, id) {
+      var count = document.createElement('div');      
+      count.setAttribute('class', 'remcharsCount');
+      count.setAttribute('id', 'remchars_' + id);
+      count.innerHTML = this.statustext;
+
+      this.insertAfter(count, element);
+  };
+
+  this.showRemaining = function(element) {
+      console.log(this);
+      var max = element.getAttribute('data-maxlen') || 0;
+      var arr = Array.prototype.slice.call(this.elements);
+      var id = arr.indexOf(element);      
+      var statusarea = document.getElementById('remchars_' + id);
+      var remaining = max - element.value.length;         
+      console.log(element);         
+
+      statusarea.innerHTML = this.statustext.replace(/%d/, remaining);
+  };
+
+  // Append counters to elements and attach event listener
+  this.load = function() {      
+      for(var i=0, len=this.elements.length; i<len; i++) {
+          var id = i;          
+          if( this.elements[i].hasAttribute('data-maxlen') ) {
+              this.appendCharCount(this.elements[i], id);
+              this.showRemaining(this.elements[i]);
+
+              if( this.elements[i].nodeName.toLowerCase() === 'input' ) {
+                  this.elements[i].onchange = function() { self.showRemaining(self.elements[i]) };
+              } 
+
+              if( this.elements[i].nodeName.toLowerCase() === 'textarea' ) {
+                  this.elements[i].onchange = function() { self.showRemaining(self.elements[i]) };
+                  this.elements[i].onkeyup = function() { self.showRemaining(self.elements[i]) };
+              } 
+          }
+      }  
+  }
+
+  // Fetch all elements
+  this.elements = document.querySelectorAll('.remchars'); 
+  if( typeof(this.elements) === 'undefined' || this.elements === null || this.elements.length <= 0 ) {
       return false;
-    }              
-    self.showStatus((self.maxChars-actualCount));
-  });           
-  
-  // Initial status message
-  this.showStatus(this.maxChars);
+  }
+
+  // Parse and set options
+  if( typeof opts.statustext === undefined || opts.statustext.search(/%d/) === -1 ) {
+      this.statustext = 'Remaining: %d';
+  } else {
+      this.statustext = opts.statustext;
+  }
 };
 
